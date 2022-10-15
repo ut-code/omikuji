5402c9b844029a646d2e89b5c74b1791f23ceaa8
waterwalker50 <104970808+waterwalker50@users.noreply.github.com>
2022-10-15 19:47:48 +0900

    説明文言の修正

--- script.js
+++ script.js
@@ -7,27 +7,29 @@ let daikichi = Number(document.getElementById("daikichi").value);
 let kyou = Number(document.getElementById("kyou").value);
 let kichi = 1 - daikichi - kyou;
 
-
-
 sendButton.onclick = () => {
   daikichi = Number(document.getElementById("daikichi").value);
   kyou = Number(document.getElementById("kyou").value);
   kichi = 1 - daikichi - kyou;
-}
-
-
+};
 
 omikujiButton.onclick = () => {
-    // この直接.srcを変える切り替え方、絶対良くない（三つ出しといて隠すとか？）
-    const r = Math.random();
-    if (r < daikichi) {
-        omikujiImage.src = "./omikuji_daikichi.png";
-        setTimeout(() => {omikujiImage.src = "./omikuji-box.png"}, 1000);
-    } else if (r < daikichi + kichi) {
-        omikujiImage.src = "./omikuji_kichi.png";
-        setTimeout(() => {omikujiImage.src = "./omikuji-box.png"}, 1000);
-    } else {
-        omikujiImage.src = "./omikuji_kyou.png";
-        setTimeout(() => {omikujiImage.src = "./omikuji-box.png"}, 1000);
-    }
+  // 切り替え方は改善必要
+  const r = Math.random();
+  if (r < daikichi) {
+    omikujiImage.src = "./omikuji_daikichi.png";
+    setTimeout(() => {
+      omikujiImage.src = "./omikuji-box.png";
+    }, 1000);
+  } else if (r < daikichi + kichi) {
+    omikujiImage.src = "./omikuji_kichi.png";
+    setTimeout(() => {
+      omikujiImage.src = "./omikuji-box.png";
+    }, 1000);
+  } else {
+    omikujiImage.src = "./omikuji_kyou.png";
+    setTimeout(() => {
+      omikujiImage.src = "./omikuji-box.png";
+    }, 1000);
+  }
 };
